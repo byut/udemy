f9cddae7b223954440813427f0be3e8c669e9922
Volodymyr But <vbut.dev+git@gmail.com>
2023-11-09 14:12:59 +0100

    Data Structures & Algorithms: New solution - Coding Exercise 1

---
+++ Data Structures & Algorithms/Exercises/001.py
@@ -0,0 +1,16 @@
+#
+# Big O: O(n)
+#
+# Implement a Python function called print_items
+#
+# This function should take a single integer as an argument and
+# print out a sequence of numbers from 0 up to, but not
+# including, the provided integer.
+#
+# Exercise: https://ua.udemy.com/course/data-structures-algorithms-python/learn/quiz/5972210
+#
+
+
+def print_items(n):
+    for i in range(n):
+        print(i)
