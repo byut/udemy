6c67594a01996b79527f15600e76a6bca6eab3b0
Volodymyr But <vbut.dev+git@gmail.com>
2023-12-19 18:54:30 +0100

    Data Structures & Algorithms: New solution - Coding Exercise 40

---
+++ Data Structures & Algorithms/Exercises/040.py
@@ -0,0 +1,13 @@
+#
+# Stack: Implement Stack Using a List
+#
+# Create a constructor for Class Stack that implements a
+# new stack with an empty list called stack_list.
+#
+# Exercise: https://ua.udemy.com/course/data-structures-algorithms-python/learn/quiz/5799342#content
+#
+
+
+class Stack:
+    def __init__(self):
+        self.stack_list = []
