6a764bbedeb585ccd36c9838e7c4e0b399ed6b4e
Volodymyr But <vbut.dev+git@gmail.com>
2023-12-25 19:59:51 +0100

    Data Structures & Algorithms: New solution - Coding Exercise 61

---
+++ Data Structures & Algorithms/Exercises/061.py
@@ -0,0 +1,14 @@
+#
+# Set: Remove Duplicates
+#
+# You have been given a list my_list with some duplicate
+# values. Your task is to write a Python program that removes
+# all the duplicates from the list using a set and then prints the
+# updated list.
+#
+# Exercise: https://ua.udemy.com/course/data-structures-algorithms-python/learn/quiz/5806096
+#
+
+
+def remove_duplicates(input):
+    return list(set(input))
