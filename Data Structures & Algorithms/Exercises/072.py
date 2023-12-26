fedf9ea408c291027ca747e5bd1d509e6ad6d70f
Volodymyr But <vbut.dev+git@gmail.com>
2023-12-26 19:47:56 +0100

    Data Structures & Algorithms: New solution - Coding Exercise 72

---
+++ Data Structures & Algorithms/Exercises/072.py
@@ -0,0 +1,32 @@
+#
+# MinHeap: Insert
+#
+# You are provided with a partial implementation of a
+# MinHeap class.
+#
+# Exercise: https://ua.udemy.com/course/data-structures-algorithms-python/learn/quiz/5969842
+#
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        index = len(self.heap) - 1
+        while index > 0 and self.heap[self._parent(index)] > self.heap[index]:
+            self._swap(self._parent(index), index)
+            index = self._parent(index)
