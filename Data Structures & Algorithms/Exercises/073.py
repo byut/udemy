149169474ccd6341645fd4eafad53d8a23527e11
Volodymyr But <vbut.dev+git@gmail.com>
2023-12-26 19:50:24 +0100

    Data Structures & Algorithms: New solution - Coding Exercise 73

---
+++ Data Structures & Algorithms/Exercises/073.py
@@ -0,0 +1,59 @@
+#
+# MinHeap: Remove
+#
+# Your task is to finalize this class by implementing the remove
+# method.
+#
+# Exercise: https://ua.udemy.com/course/data-structures-algorithms-python/learn/quiz/5969844
+#
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if (
+                left_index < len(self.heap)
+                and self.heap[left_index] > self.heap[max_index]
+            ):
+                max_index = left_index
+
+            if (
+                right_index < len(self.heap)
+                and self.heap[right_index] > self.heap[max_index]
+            ):
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        return max_value
